18732213202b0828055a4cff1178978305747b7b
Amir Akbarzadeh <amir.akbarzadh3@gmail.com>
2025-08-16 13:06:38 +0330

    update ci/cd

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const {createServer} = require('http')
-const {parse} = require('url')
+const { createServer } = require('http')
+const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000
-const app = next({dev})
+const port = process.env.PORT || 3000;
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -12,7 +12,7 @@ app.prepare().then(() => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
-    const {pathname, query} = parsedUrl
+    const { pathname, query } = parsedUrl
 
     if (pathname === '/a') {
       app.render(req, res, '/a', query)
